cb34ac5f6c37979963925b7bd1e96888b079903d
ProVid-ir <32803076+ProVid-ir@users.noreply.github.com>
2017-10-14 14:06:15 -0700

    Add files via upload

---
+++ MyFreeCoreTutorialProVid/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyFreeCoreTutorialProVid.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
